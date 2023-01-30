4199115de9bd48def724941d854778c0ed0f3f99
DandinPower <tomhot246@gmail.com>
2023-01-30 09:46:36 +0800

    fid token count feature

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,7 @@
 from python.libs.statistic import Entries
 from python.model.agent import Agent, InferenceAgent
 from python.environment.trace_loader import TraceLoader
+from python.environment.tokenizer import FidTokenizer
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -10,6 +11,20 @@ TRACE_FILE_PATH = os.getenv('TRACE_FILE_PATH')
 TRACE_LOAD_LENGTH = int(os.getenv('TRACE_LOAD_LENGTH'))
 LBA_SIZE = int(os.getenv('LBA_SIZE'))
 PAGE_SIZE = 4096
+
+def CheckFidFrequency():
+    traceLoader = TraceLoader()
+    tokenizer = FidTokenizer()
+    traceLoader.Load(TRACE_FILE_PATH, TRACE_LOAD_LENGTH)
+    for i in range(len(traceLoader)):
+        trace = traceLoader.GetWriteTrace()
+        token = tokenizer[trace._fid]
+        if i % 999 == 0:
+            for key, value in tokenizer.datas.items():
+                print(f'fid: {key}, id: {value.id}, count: {value.count}')
+            print('//////////////////////')
+            tokenizer.ResetAll()
+    #print(tokenizer.datas)
 
 def GetTargetAnswer():
     traceLoader = TraceLoader()
@@ -21,8 +36,9 @@ def GetTargetAnswer():
     entries.Write(TARGET_ANSWER_PATH)
 
 def main():
-    agent = InferenceAgent()
-    agent.Inference()
+    #agent = InferenceAgent()
+    #agent.Inference()
+    CheckFidFrequency()
 
 if __name__ == "__main__":
     main()
--- python/environment/tokenizer.py
+++ python/environment/tokenizer.py
@@ -1,11 +1,18 @@
+from collections import namedtuple
+
+FidTokenData = namedtuple('FidTokenData', ['id', 'count'])
+
 class FidTokenizer:
     def __init__(self):
         self.datas = dict()
     
     def __getitem__(self, key):
-        if self.datas.get(key) == None:
-            self.datas[key] = len(self.datas)
-        return self.datas[key]
+        temp = self.datas.get(key)
+        if  temp == None:
+            self.datas[key] = FidTokenData(id = len(self.datas), count = 0)
+        else:
+            self.datas[key] = temp._replace(count = temp.count + 1)
+        return self.datas[key].id
 
     def ResetAll(self):
         self.datas.clear()
